0574831e1295133a3fa7b07b0d3fef0a52081eab
Benjamin Jenni <benjamin.jenni@secacon.com>
2017-05-11 12:10:55 +0200

    Add variables and input validation

--- sp_users.sql
+++ sp_users.sql
@@ -1,6 +1,32 @@
 DROP PROCEDURE IF EXISTS `sp_users`;
 
-CREATE PROCEDURE `sp_users`()
-  BEGIN
+CREATE PROCEDURE `sp_users`(
+  IN benutzer       VARCHAR(255),
+  IN passwort       VARCHAR(255),
+  IN hostname       VARCHAR(255),
+  IN berechtigungen VARCHAR(255),
+  IN zugriffsort    VARCHAR(255)
+)
 
-  END;
+  BEGIN
+    /* Input Validation */
+    IF (ISNULL(benutzer, '') = '')
+    THEN
+      SELECT "Ungültige Eingabe: Benutzer darf nicht leer sein!";
+    END IF;
+    IF (ISNULL(passwort, '') = '') THEN
+      SELECT "Ungültige Eingabe: Benutzer darf nicht leer sein!";
+    END IF;
+    IF (ISNULL(hostname, '') = '') THEN
+      SELECT "Ungültige Eingabe: Hostname darf nicht leer sein!";
+    END IF;
+    IF (ISNULL(berechtigungen, '') = '') THEN
+      SELECT "Ungültige Eingabe: Berechtigungen dürfen nicht leer sein!";
+    END IF;
+    IF (ISNULL(zugriffsort, '') = '') THEN
+      SELECT "Ungültige Eingabe: Zugriffsort darf nicht leer sein!";
+    END IF;
+
+    /**/
+    
+END;
